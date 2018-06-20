f8a2fc654da5ab1f5c1cc3fc5c94b38ded2799c5
Destiny <richard@ubuntu.(none)>
2018-06-20 07:41:19 -0700

    [modify]recommit tcp dir.

--- c/app/tcp_ip/tcp_command/client.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CLIENT_H_
-#define _CLIENT_H_
-#include <semaphore.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<fcntl.h>
-extern sem_t sem_stream;
-int data_pthread(char *argv[]);
-#endif
